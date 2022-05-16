8c71617e0ed5a5575eb6d346ec003728e2857bdb
osakanara <47262417+osakanara@users.noreply.github.com>
2022-05-17 02:42:44 +0900

    CSSを定義

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import React from "react";
+import "./styles.css";
 
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了リスト</p>
+      <div className="incomplete-area">
+        <p className="title">未完了リスト</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li> TODOする1</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li> TODOする2</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>完了リスト</p>
+      <div className="complete-area">
+        <p className="title">完了リスト</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li> TODO済み</li>
             <button>戻す</button>
           </div>
