65e4daaadb208151b7c6eba47ff42004ceab7ce9
osakanara <47262417+osakanara@users.noreply.github.com>
2022-05-21 16:51:20 +0900

    完了ボタンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,17 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const currentIncompleteTodos = [...incompleteTodos];
+    const completeTodoText = currentIncompleteTodos[index];
+    // 完了リストに要素を追加
+    const newCompleteTodos = [...completeTodos, completeTodoText];
+    setCompleteTodos(newCompleteTodos);
+    // 未完了リストから要素を削除
+    currentIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(currentIncompleteTodos);
+  };
+
   const [incompleteTodos, setIncompleteTodos] = useState([
     "TODOする１",
     "TODOする２"
@@ -45,7 +56,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
