cc8239eb6b0f16998e4ed9e412835c5428f25e18
osakanara <47262417+osakanara@users.noreply.github.com>
2022-05-21 16:37:23 +0900

    削除ボタンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   const [incompleteTodos, setIncompleteTodos] = useState([
     "TODOする１",
     "TODOする２"
@@ -35,12 +41,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了リスト</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
